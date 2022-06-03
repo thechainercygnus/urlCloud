f286977a062fe9d22eb8924e4aa33530cddade81
Bryce <bryce@durish.xyz>
2022-06-02 21:11:59 -0600

    Addressed SQLAlchemy 1.4 Plugin Change

--- urlcloud/database.py
+++ urlcloud/database.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import sessionmaker
 
 from .config import get_settings
 
-engine = create_engine(get_settings().db_url, connect_args={"check_same_thread": False})
+uri = get_settings().db_url
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+engine = create_engine(uri, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
